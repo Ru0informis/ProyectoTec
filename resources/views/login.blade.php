45087607763e5d1938b1d1907cd168a6a73b90ec
Lennincitalan <lennin.pumas@gmail.com>
2021-05-26 20:36:29 -0500

    Modifique login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="{{ asset('static/css/style.css') }}">
+    <title>Bienvenido</title>
+    
+</head>
+<body>
+    @if(session('error'))
+    <div>
+        {{session('error')}}
+    </div>
+    @endif
+    <form class="form_login" action="/validar" method="POST">
+    @csrf
+    <div class="form_title">Bienvenido uwuwuwuw</div>
+    <div>
+    <center><label>Nombre</label></center>
+    <input class="form_user" type="text" name="name">
+    </div>
+    <div>
+    <center><label >Contraseña</label></center>
+    
+    <input class="form_user" type="password" name="pass">
+    </div>
+    <input type="submit" class="btnLog" value="LogIN"">
+    </form>
+    
+</body>
+</html>
+
+
