63f0037cc18dd761cf4cfd53c19b853d867b736b
Lennincitalan <lennin.pumas@gmail.com>
2021-05-27 18:34:38 -0500

    Cambie el estio de login, facherito facherito

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,32 +4,35 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <link rel="stylesheet" href="{{ asset('static/css/style.css') }}">
+    <link rel="stylesheet" href="static/css/estilo.css">
     <title>Bienvenido uwu</title>
     
 </head>
+
 <body>
     @if(session('error'))
-    <div>
         {{session('error')}}
-    </div>
     @endif
-    <form class="form_login" action="/validar" method="POST">
-    @csrf
-    <div class="form_title">Bienvenido uwu</div>
-    <div>
-    <center><label>Nombre</label></center>
-    <input class="form_user" type="text" name="name">
-    </div>
-    <div>
-    <center><label >Contraseña</label></center>
     
-    <input class="form_user" type="password" name="pass">
+    <div class = "login-box">
+        <img class= "avatar" src="static/logo.png" alt = "Logo">
+
+            <h1> Bienvenido</h1>
+            <form class="form_login" action="/validar" method="POST">
+            @csrf
+                <label for="username"> Usuario</label>
+                <input type="text" name="name" placeholder="Ingrese Usuario">
+
+                <label for="password"> Contraseña</label>
+                <input type="password" name="pass" placeholder="Ingrese contraseña">
+
+                <button class="btn" type="submit" name="btnIniciar" value="login">
+                     Ingresar
+                </button>
+
+            </form>
+            
     </div>
-    <input type="submit" class="btnLog" value="LogIN"">
-    </form>
-    
 </body>
 </html>
 
