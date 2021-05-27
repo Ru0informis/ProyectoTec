44cb5184cf22d1b815d103be6c430b38222991cf
jose david morales perez <63940282+Ru0informis@users.noreply.github.com>
2021-05-26 20:44:55 -0500

    cambie la cabecera

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" href="{{ asset('static/css/style.css') }}">
-    <title>Bienvenido</title>
+    <title>Bienvenido uwu</title>
     
 </head>
 <body>
