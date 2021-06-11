b9fd677e89ffea40aede3aa68bf7f8b23753d64f
Lennincitalan <lennin.pumas@gmail.com>
2021-06-11 18:24:57 -0500

    ggg

--- resources/views/verHistorial.blade.php
+++ resources/views/verHistorial.blade.php
@@ -3,15 +3,12 @@
 @section('verHistorial')
 <?php $i=0; ?>
     @foreach ($historial as $historial )
-    
-    @if ($i==0)
-    
-    Fecha de registro del vendedor: {{$historial->cuando}}
-
-    @endif
+        @if ($i==0)
+            Fecha de registro del vendedor: {{$historial->cuando}} <br>
+        @endif
         
-        productos vendidos: {{$historial->producto}}
-
+        productos vendidos: {{$historial->producto}} <br>
+        <?php $i=1; ?> 
     @endforeach
     
 
